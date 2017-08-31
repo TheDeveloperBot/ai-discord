ce56c912eb1486ed9e571398a94e5b7679bde29e
meldsza <melroynd@live.com>
2017-08-31 23:51:58 +0530

    immortal bot

--- src/pandorabot.js
+++ src/pandorabot.js
@@ -1,8 +1,14 @@
 const request = require('request-promise-native');
 const xml = require('./xml2js');
-let sessionids = {};
+try {
+    let sessionids = require('./sessions.json');
+}
+catch (e) {
+    sessionids = {};
+}
 const bot_url = settings.bot_url || "https://www.pandorabots.com/pandora/talk-xml?botid=a49104941e378378&";
 const querystring = require('querystring');
+const fs = require('fs');
 module.exports = async function (question, client_name) {
     let formdata = {
         input: question
@@ -11,6 +17,7 @@ module.exports = async function (question, client_name) {
         formdata.custid = sessionids[client_name];
     }
     else {
+        setTimeout(saveSession, 2000)
         formdata.input = "My name is " + client_name + ". " + formdata.input
     }
     let res = await request(bot_url + querystring.stringify(formdata))
@@ -21,3 +28,7 @@ module.exports = async function (question, client_name) {
     sessionids[client_name] = res.$.custid;
     return res.that[0].replace(/<br>/g, "\n");
 }
+function saveSession() {
+    fs.writeFile("sessions.json", JSON.stringify(sessionids), () => console.log("saved"));
+}
+saveSession()
